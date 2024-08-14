c78c96d49c01036971c6dba140d4aac863cbc0f4
Guilherme Rodrigues <oliveirack1@gmail.com>
2024-08-14 18:38:33 -0300

    add laravel

---
+++ resources/views/home.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Guilherme Rodrigues</title>
+    <link rel="stylesheet" href="/css/app.css" />
+</head>
+
+<body">
+    <h1 class="font-black">Lorem ipsum</h1>
+    <div class="flex-direction">
+        <section class="background-color">
+            <h1>section title</h1>
+            <p>section first paragraph</p>
+            <p>section second paragraph</p>
+        </section>
+        <section class="background-color">
+            <h1>section title</h1>
+            <p>section first paragraph</p>
+            <p>section second paragraph</p>
+        </section>
+    </div>
+    </body>
+
+</html>
