c3fbf861ac99cf484d64bad19f35449fc5709311
Guilherme Rodrigues <oliveirack1@gmail.com>
2024-09-05 17:00:52 -0300

    new container

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-window.addEventListener("load", function () {
-  setTimeout(function () {
-    alert("Portfólio em construção");
-  }, 100);
-});
+// window.addEventListener("load", function () {
+//   setTimeout(function () {
+//     alert("Portfólio em construção");
+//   }, 100);
+// });
